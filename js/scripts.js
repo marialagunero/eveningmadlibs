bcf510702a0ef0945ac13bd9e51d2ae2d604efbf
Student <example@example.com>
2019-11-18 20:46:52 -0800

    add text to story, css and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,13 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var firstnameInput = $("input#firstname").val();
+    var addressInput = $("input#address").val();
+    var favoritefoodInputood = $("input#favoritefood").val();
 
-    $(".person1").append(person1Input);
-    $(".person2").append(person2Input);
-    $(".animal").append(animalInput);
-    $(".exclamation").append(exclamationInput);
-    $(".verb").append(verbInput);
-    $(".noun").append(nounInput);
+    $(".firstname").text(firstnameInput);
+    $(".address").text(addressInput);
+    $(".favoritefood").text(favoritefoodInput);
+
 
     $("#story").show();
 
